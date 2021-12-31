c2fd626a21cf5138d6690f137aaae6382cc69d44
cvlvxi <cvlvxi@outlook.com>
2021-12-31 12:20:24 +1100

    sizze constraints on lottie animation

--- app/src/main/java/com/cvlvxi/animedroid/LottieAnimations.kt
+++ app/src/main/java/com/cvlvxi/animedroid/LottieAnimations.kt
@@ -1,19 +1,27 @@
 package com.cvlvxi.animedroid
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.*
 
 
 @Preview
 @Composable
 fun MyLottieAnim01() {
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.catwalk))
-    val progress by animateLottieCompositionAsState(composition)
-    LottieAnimation(
-        composition,
-//        progress,
-        iterations = LottieConstants.IterateForever
-    )
+
+    Box(modifier=Modifier.fillMaxSize()) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.catwalk))
+        LottieAnimation(
+            composition,
+            iterations = LottieConstants.IterateForever,
+            modifier = Modifier.width(100.dp).height(300.dp)
+        )
+    }
 }

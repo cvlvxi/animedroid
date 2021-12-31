d9c59346624104683f6a927e00308288831426f3
cvlvxi <cvlvxi@outlook.com>
2021-12-31 12:32:55 +1100

    Combine Lottie with Animate IntOffset Animation to update offset to make thing move

--- app/src/main/java/com/cvlvxi/animedroid/LottieAnimations.kt
+++ app/src/main/java/com/cvlvxi/animedroid/LottieAnimations.kt
@@ -1,27 +1,36 @@
 package com.cvlvxi.animedroid
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.*
 
 
-@Preview
 @Composable
-fun MyLottieAnim01() {
-
-    Box(modifier=Modifier.fillMaxSize()) {
+fun MyLottieAnim01(selected: Boolean = false, onClick: () -> Unit) {
+    Box(modifier=Modifier.fillMaxSize().clickable { onClick() }) {
+        val intOffset: IntOffset by animateIntOffsetAsState(
+            if (!selected) IntOffset(0, 0) else IntOffset(200, 0),
+            animationSpec = tween(
+                durationMillis = 5000, 1000
+            )
+        )
         val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.catwalk))
         LottieAnimation(
             composition,
             iterations = LottieConstants.IterateForever,
-            modifier = Modifier.width(100.dp).height(300.dp)
+            modifier = Modifier
+                .width(100.dp)
+                .height(300.dp)
+                .offset(x=intOffset.x.dp, y=intOffset.y.dp)
         )
     }
 }
--- app/src/main/java/com/cvlvxi/animedroid/MainActivity.kt
+++ app/src/main/java/com/cvlvxi/animedroid/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cvlvxi.animedroid.ui.theme.AnimedroidTheme
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyLottieAnim01()
+                    var selected by remember {mutableStateOf(false)}
+                    MyLottieAnim01(selected=selected, onClick={ selected=!selected})
                 }
             }
         }
